e39276d9d0bf492b81e8933c53fbb79e517f113e
Gustavo Krüger <gustavo.kruger@accenture.com>
2018-01-16 07:20:04 -0300

    complete initial version of db and modeldb

--- Dump.sql
+++ Dump.sql
@@ -35,6 +35,49 @@ CREATE TABLE `Endereco` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_franchise` int(11) DEFAULT NULL,
+  `id_pessoa` int(11) DEFAULT NULL,
+  `funcao` varchar(100) DEFAULT NULL,
+  `data_admissao` datetime DEFAULT NULL,
+  `data_demissao` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_pessoa_client_idx` (`id_pessoa`),
+  KEY `id_franchise_client_idx` (`id_franchise`),
+  CONSTRAINT `id_franchise_client` FOREIGN KEY (`id_franchise`) REFERENCES `franchise` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_pessoa_client` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `condutor`
+--
+
+DROP TABLE IF EXISTS `condutor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `condutor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pessoa` int(11) DEFAULT NULL,
+  `data_nascimento` varchar(45) DEFAULT NULL,
+  `cnh` varchar(100) DEFAULT NULL,
+  `cnh_validade` datetime DEFAULT NULL,
+  `cnh_cidade` varchar(200) DEFAULT NULL,
+  `cnh_estado` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_pessoa_condutor_idx` (`id_pessoa`),
+  CONSTRAINT `id_pessoa_condutor` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `foto`
 --
 
@@ -143,6 +186,59 @@ CREATE TABLE `pessoa` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(100) DEFAULT NULL,
+  `value` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tag_client`
+--
+
+DROP TABLE IF EXISTS `tag_client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag_client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tag` int(11) DEFAULT NULL,
+  `id_client` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_tag_tag_client_idx` (`id_tag`),
+  KEY `id_client_tag_client_idx` (`id_client`),
+  CONSTRAINT `id_client_tag_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_tag_tag_client` FOREIGN KEY (`id_tag`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tag_veiculo`
+--
+
+DROP TABLE IF EXISTS `tag_veiculo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag_veiculo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tag` int(11) DEFAULT NULL,
+  `id_veiculo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_tag_tag_veiculo_idx` (`id_tag`),
+  KEY `id_veiculo_tag_veiculo_idx` (`id_veiculo`),
+  CONSTRAINT `id_tag_tag_veiculo` FOREIGN KEY (`id_tag`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_veiculo_tag_veiculo` FOREIGN KEY (`id_veiculo`) REFERENCES `veiculo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `telefone`
 --
 
@@ -170,8 +266,62 @@ CREATE TABLE `telefone_pessoa` (
   PRIMARY KEY (`id`),
   KEY `id_telefone_idx` (`id_telefone`),
   KEY `id_pessoa_idx` (`id_pessoa`),
-  CONSTRAINT `id_telefone_telefone_pessoa` FOREIGN KEY (`id_telefone`) REFERENCES `telefone` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `id_pessoa_telefone_pessoa` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `id_pessoa_telefone_pessoa` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_telefone_telefone_pessoa` FOREIGN KEY (`id_telefone`) REFERENCES `telefone` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `veiculo`
+--
+
+DROP TABLE IF EXISTS `veiculo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `veiculo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) DEFAULT NULL,
+  `chassi` varchar(200) DEFAULT NULL,
+  `tipo` varchar(200) DEFAULT NULL,
+  `ano` varchar(45) DEFAULT NULL,
+  `fabricante` varchar(200) DEFAULT NULL,
+  `modelo` varchar(200) DEFAULT NULL,
+  `modelo_extra` varchar(200) DEFAULT NULL,
+  `placa` varchar(45) DEFAULT NULL,
+  `cidade` varchar(200) DEFAULT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `proprietario` varchar(200) DEFAULT NULL,
+  `cor` varchar(45) DEFAULT NULL,
+  `carroceria` varchar(100) DEFAULT NULL,
+  `carroceria_subtipo` varchar(100) DEFAULT NULL,
+  `valor` varchar(45) DEFAULT NULL,
+  `id_foto` int(11) DEFAULT NULL,
+  `id_franchise` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_foto_veiculo_idx` (`id_foto`),
+  KEY `id_franchise_veiculo_idx` (`id_franchise`),
+  CONSTRAINT `id_foto_veiculo` FOREIGN KEY (`id_foto`) REFERENCES `foto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_franchise_veiculo` FOREIGN KEY (`id_franchise`) REFERENCES `franchise` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `veiculo_client`
+--
+
+DROP TABLE IF EXISTS `veiculo_client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `veiculo_client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_veiculo` int(11) DEFAULT NULL,
+  `id_client` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_veiculo_veiculo_cliente_idx` (`id_veiculo`),
+  KEY `id_cliente_veiculo_client_idx` (`id_client`),
+  CONSTRAINT `id_client_veiculo_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_veiculo_veiculo_client` FOREIGN KEY (`id_veiculo`) REFERENCES `veiculo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -184,4 +334,4 @@ CREATE TABLE `telefone_pessoa` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-15  7:28:18
+-- Dump completed on 2018-01-16  7:19:01
